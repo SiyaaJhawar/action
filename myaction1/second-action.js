2454d1882bd990a9c01b294316575b2eb0d4f903
Gupta <swgu@softwareag.com>
2023-03-29 11:26:28 +0530

    msg

--- myaction1/second-action.js
+++ myaction1/second-action.js
@@ -1,1 +1,33 @@
-console.log("hi,how are you?");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+ 
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.publicapis.org/entries", true);
+xhr.send();
+ 
+xhr.onreadystatechange = function () {
+  console.log("readyState = " + this.readyState + ", status = " + this.status);
+  if (this.readyState === 4 && this.status === 200) {
+    var result = JSON.parse(this.responseText);
+    console.log(result);
+
+    for (var key in result) {
+      if (result.hasOwnProperty(key)) {
+        var val = result[key];
+        console.log(result.hasOwnProperty(key));
+        if (Array.isArray(val)) {
+          val.forEach(function(item) {
+            for (var k in item) {
+              if (item.hasOwnProperty(k)) {
+            
+                console.log(k + ": " + item[k]);
+              } 
+              }
+            });
+          }
+        else {
+          console.log(key + ": " + val);
+        }
+      }
+    }
+  }
+};
