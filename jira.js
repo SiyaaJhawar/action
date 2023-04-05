83744d2363093b6e9189a766ab3a530a347d4b51
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-05 12:07:00 +0530

    Update jira.js

--- jira.js
+++ jira.js
@@ -2,7 +2,7 @@ import { XMLHttpRequest } from "xmlhttprequest";
 
  
 var xhr = new XMLHttpRequest();
-var username = "swatg139@gmail.com";
+var username = "swatig139@gmail.com";
 var apiToken = "ATATT3xFfGF0mqSJBvyjdoYaK7MSraNbWxCKhCD38fo80eo_GTkgyX6oLGRQ8HmiOHaG0axUg0uexz7UhY6_Acl297byxTRK5H7-CCuqPVOCKvdQZM1nn9OnmFV_ZSBqRkVUfgIQBj-NhaB1R35Cw1MXx2Z0noVPapKlTVVDhdAElbHVYaWd2HQ=60DEC96F";
 xhr.open("GET", "https://swgup.atlassian.net/rest/api/3/search?filter=allissues", true);
 xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + apiToken));
