6b6819c8489f3c62dadd4f9662c997cca13b8429
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 14:54:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const octokit = new Octokit({
-  auth: 'YOUR-TOKEN'
+  auth: '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'
 })
 
 await octokit.request('GET /repos/{owner}/{repo}/git/commits/b63d854b9c2d60ba843e7da1106c8076fe203704}', {
