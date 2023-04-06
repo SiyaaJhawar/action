c67ba03cc017deb07c705c3b5912f59e3ebf20f3
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 15:01:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
-const octokit = new Octokit({
-  auth: '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'
-})
+const owner = "SiyaaJhawar";
+const repo = "action";
+const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
+const url = `https://api.github.com/repos/SiyaaJhawar/action/commits/b63d854b9c2d60ba843e7da1106c8076fe203704/comments`;
 
-await octokit.request('GET /repos/{owner}/{repo}/git/commits/b63d854b9c2d60ba843e7da1106c8076fe203704}', {
-  owner: 'SiyaaJhawar',
-  repo: 'action',
-  comment_id: 'b63d854b9c2d60ba843e7da1106c8076fe203704',
+fetch(url, {
   headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-    'Accept: application/vnd.github+json'
+    Authorization: "Bearer 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",
+    Accept: "application/vnd.github.v3+json"
   }
 })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Do something with the data
+  })
+  .catch(error => console.error(error));
