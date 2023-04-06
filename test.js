7786986ef47f5cabdf986341f91ca6857994b70d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 15:05:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const owner = "SiyaaJhawar";
 const repo = "action";
 const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
-const url = `https://api.github.com/repos/SiyaaJhawar/action/commits/b63d854b9c2d60ba843e7da1106c8076fe203704/comments`;
+const url = `https://api.github.com/repos/SiyaaJhawar/action/git/commits/b63d854b9c2d60ba843e7da1106c8076fe203704`;
 
 fetch(url, {
   headers: {
