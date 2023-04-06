906a2b91480479dcb55dc0349b3f6b456daae9aa
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 16:54:53 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ const owner = "SiyaaJhawar";
 const repo = "action";
 const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
 const url = `https://api.github.com/repos/SiyaaJhawar/action/git/commits/b63d854b9c2d60ba843e7da1106c8076fe203704`;
+const token = "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"
 
 fetch(url, {
   headers: {
     
-    "Authorization" : `Basic ${btoa("SiyaaJhawar:Siyaa@12345")}`,
-    
-     "Accept" : 'application/vnd.github.v3+json'
+    "Authorization": `Token ${token}`,
+    "Accept": "application/vnd.github.v3+json"
   }
 
 })
