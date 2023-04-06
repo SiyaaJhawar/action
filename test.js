7ba17fe7086423a30485d2949cf32255bc2c479d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 17:00:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const repo = "action";
 const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
 const url = `https://api.github.com/repos/SiyaaJhawar/action/git/commits/b63d854b9c2d60ba843e7da1106c8076fe203704`;
 const username = "SiyaaJhawar";
-const password = "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";
+const password = "Siyaa@12345";
 
 fetch(url, {
   headers: {
