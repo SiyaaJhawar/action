2a8ccde7e0e7c3864e2e06ca541cc92a914f5d6e
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-06 17:34:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ const owner = "SiyaaJhawar";
 const repo = "action";
 const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
 const url =  `https://api.github.com/repos/SiyaaJhawar/action/commits/7ba17fe7086423a30485d2949cf32255bc2c479d/comments`;
-const username = "SiyaaJhawar";
-const password = "Siyaa@12345";
+const username = process.env.GITHUB_USERNAME;
+const password = process.env.GITHUB_API_TOKEN;
 
 fetch(url, {
   headers: {
