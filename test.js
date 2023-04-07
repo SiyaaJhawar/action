a3b54fd5fd194c8ba05ebc11c9d6b28ec420900a
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-07 10:45:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,9 +3,9 @@ const repo = "action";
 const commit_sha = "b63d854b9c2d60ba843e7da1106c8076fe203704";
 const url =  `https://api.github.com/repos/SiyaaJhawar/action/commits/7ba17fe7086423a30485d2949cf32255bc2c479d/comments`;
 #const username = process.env.GITHUB_USERNAME;
-const username = $secrets.GITHUB_USERNAME;
+const username = '${{ secrets.GITHUB_USERNAME }}';
+const password = '${{ secrets.GITHUB_API_TOKEN }}';
 
-const password = $secrets.GITHUB_API_TOKEN;
 #const password = process.env.GITHUB_API_TOKEN;
 
 fetch(url, {
