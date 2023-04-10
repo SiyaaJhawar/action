5fa2d402a1ceca092d4a5e6323b04a4aa3b2d886
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-10 11:51:07 +0530

    Update test.js

--- test.js
+++ test.js
@@ -19,7 +19,8 @@ fetch(url, {
   .then(data => {
     console.log(data);
   const commentTexts = data.map(comment => comment.body);
- const defectRegex = /^[^-]*/g
+ const defectRegex = /^[A-Z0-9]+(?=-)/g
+
  const defectIds = commentTexts.flatMap(text => {
   const matches = [];
   let match;
