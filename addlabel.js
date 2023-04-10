a3ab4c3f9ef2dadae9ad7d7ede67888f2ae4b2b0
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-10 15:19:31 +0530

    Create addlabel.js

---
+++ addlabel.js
@@ -0,0 +1,56 @@
+const JiraApi = require('jira-connector');
+
+const defectRegex = /([A-Z]{3})-\d+/g;
+const jiraUrl = '';
+const jiraAuth = {
+  username: '<YOUR_JIRA_USERNAME>',
+  password: '<YOUR_JIRA_PASSWORD>'
+};
+
+const commentTexts = [WFL-1105, WFL-101, SWT-1, SWT-2, WFL-1015, WFL-1010, WFL-1001, WFL-1002, CLDP-1003, WFL-1001, WFL-1101];
+const defectIds = commentTexts.flatMap(text => {
+  const matches = [];
+  let match;
+  while ((match = defectRegex.exec(text))) {
+    matches.push([match[1], match[2]]);
+  }
+  return matches;
+});
+
+defectIds.forEach(defectId => {
+  const jira = new JiraApi({
+    host: jiraUrl,
+    basic_auth: jiraAuth
+  });
+
+  jira.search.search({
+    jql: `key=${defectId[0]}-${defectId[1]}`,
+    fields: ['labels']
+  }, function(error, searchResult) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    const issues = searchResult.issues;
+    if (issues.length === 0) {
+      console.log(`Issue ${defectId[0]}-${defectId[1]} not found`);
+      return;
+    }
+
+    const issue = issues[0];
+    const labels = issue.fields.labels;
+    if (!labels.includes('int_deploy')) {
+      labels.push('int_deploy');
+
+      jira.issue.editIssue({
+        issueKey: issue.key,
+        issue: {
+          fields: {
+            labels: labels
+          }
+        }
+      }, function(error, updatedIssue) {
+        if (error) {
+          console.error(error);
+       
