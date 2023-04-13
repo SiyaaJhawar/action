fb2474c2a53dbd58a8f5b81774ede10cc9e0d6e3
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-13 18:47:57 +0530

    Update addlabel.js

--- myaction5/addlabel.js
+++ myaction5/addlabel.js
@@ -1,30 +1,27 @@
 
+import axios from 'axios';
 import fetch from 'node-fetch';
-import * as core from '@actions/core';
-import * as github from '@actions/github';
 
-
+const githubUrl = 'https://api.github.com/repos/SiyaaJhawar/action/commits/7ba17fe7086423a30485d2949cf32255bc2c479d/comments';
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraapitoken = process.env.JIRA_API_TOKEN;
-
-const token = process.env.GITHUB_TOKEN;
-const octokit = github.getOctokit(token);
-
- console.log(`Apitoken: ${token}`);
+const username = process.env.GITHUB_USERNAME;
+const password = process.env.GITHUB_API_TOKEN;
 
 const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
 
 async function compareCommitCommentWithJiraIssue() {
   try {
-  const actionResult = require('./myaction4/test.js');
-  const { encodedCredentials, commitsResponse } = actionResult;
-  // the rest of the code using encodedCredentials and commitsResponse
+    const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
+    const commitsResponse = await axios.get(githubUrl, {
+      headers: {
+        "Authorization": `Basic ${encodedCredentials}`,
+        "Accept": "application/json"
+      }
+    });
 
-    }
- catch (error) {
-  console.error('Error running action:', error);
-}
-
+    const commentTexts = commitsResponse.data.map(comment => comment.body);
+    const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
     console.log(`Found the following defect IDs in commit comments: ${defectIds}`);
     console.log(`Username: ${jiraUsername}`);
     console.log(`Apitoken: ${jiraapitoken}`);
@@ -102,6 +99,7 @@ async function compareCommitCommentWithJiraIssue() {
   }
 }
 compareCommitCommentWithJiraIssue();
+
 
 
 
