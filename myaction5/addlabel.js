f85e2b01bf48a9d0ee2416248de6f5920bb7f683
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-13 15:03:18 +0530

    Update addlabel.js

--- myaction5/addlabel.js
+++ myaction5/addlabel.js
@@ -7,6 +7,9 @@ const jiraUsername = process.env.JIRA_USERNAME;
 const jiraapitoken = process.env.JIRA_API_TOKEN;
 const username = process.env.GITHUB_USERNAME;
 const password = process.env.GITHUB_API_TOKEN;
+const token = process.env.GITHUB_TOKEN;
+const octokit = github.getOctokit(token);
+
 
 const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
 
