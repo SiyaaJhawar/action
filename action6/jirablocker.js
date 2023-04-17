40a650f18e9175366f611d17d70650c3bec0b9d9
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-17 22:35:36 +0530

    Update jirablocker.js

--- action6/jirablocker.js
+++ action6/jirablocker.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraApiToken = process.env.JIRA_API_TOKEN;
 
-const jql = 'project=SWT AND filter=10001';
+const jql = 'project=SWT AND filter=10003';
 const jira_url = 'https://swgup.atlassian.net/rest/api/3/search';
 const auth = 'Basic ' + Buffer.from(`${jiraUsername}:${jiraApiToken}`).toString('base64');
 
