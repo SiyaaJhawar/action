0411370df5fe22c19b2240e9b49405a21507a7f1
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-17 15:16:08 +0530

    Update jirablocker.js

--- action6/jirablocker.js
+++ action6/jirablocker.js
@@ -1,30 +1,29 @@
-import fetch from 'node-fetch'; 
+
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraapitoken = process.env.JIRA_API_TOKEN;
 
-const jiraUrl = 'https://swgup.atlassian.net/rest/api/3/search';
-const jiraParams = {
-  jql: 'filter=allissues AND labels=blocker', 
-  maxResults: 0, 
+const jql_blocker = 'filter=allissues and labels=blocker';
+const jql_all = 'filter=allissues';
+const jira_url = 'https://swgup.atlassian.net/rest/api/3/search';
+const auth = 'Basic ' + btoa('username:password');
+const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
+
+const checkIssues = async () => {
+  try {
+    const response1 = await fetch(`${jira_url}?jql=${jql_blocker}`, { headers });
+    const blocker_issues = (await response1.json()).total;
+
+    const response2 = await fetch(`${jira_url}?jql=${jql_all}`, { headers });
+    const all_issues = (await response2.json()).total;
+
+    if (blocker_issues > 0 || all_issues > 0) {
+      console.log('No go');
+    } else {
+      console.log('Go');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-// Fetch the Jira issues using the API
-fetch(`${jiraUrl}?${new URLSearchParams(jiraParams)}`, {
-  headers: {
-    'Authorization': 'Basic ' + Buffer.from('jiraUsername:jiraapitoken').toString('base64'), // replace with your Jira email and API token
-    'Content-Type': 'application/json',
-  },
-})
-.then(response => response.json())
-.then(data => {
-  const totalIssues = data.total;
-  if (totalIssues > 0) {
-    console.log(`There are ${totalIssues} Jira issues with the "blocker" label.`);
-  } else {
-    console.log('There are no Jira issues with the "blocker" label.');
-    console.log('go');
-  }
-})
-.catch(error => {
-  console.error('Error fetching Jira issues:', error);
-});
+checkIssues();
