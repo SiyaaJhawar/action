9c1dfd8e080793a7ca1cbb82213e7ba2a4894152
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-17 18:02:49 +0530

    Update jirablocker.js

--- action6/jirablocker.js
+++ action6/jirablocker.js
@@ -4,25 +4,20 @@ import fetch from 'node-fetch';
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraApiToken = process.env.JIRA_API_TOKEN;
 
-
-const jql_blocker = 'filter=allissues and label=blocker';
-const jql_all = 'filter=allissues';
-const jira_url = 'https://swgup.atlassian.net/rest/api/3/search?filter=allissues';
-const auth = 'Basic ' + Buffer.from('jiraUsername:JiraApiToken').toString('base64');
+const jql = 'project=SWT AND labels=blocker';
+const jira_url = 'https://swgup.atlassian.net/rest/api/3/search';
+const auth = 'Basic ' + Buffer.from('jiraUsername:jiraApiToken').toString('base64');
 const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
 
 const checkIssues = async () => {
   try {
-    const response1 = await fetch(`${jira_url}?jql=${jql_blocker}`, { headers });
-    const blocker_issues = (await response1.json()).total;
+    const response = await fetch(`${jira_url}?jql=${jql}`, { headers });
+    const issues = (await response.json()).issues;
 
-    const response2 = await fetch(`${jira_url}?jql=${jql_all}`, { headers });
-    const all_issues = (await response2.json()).total;
-
-     if (blocker_issues === 1 && all_issues > 1) {
-      console.log('Go');
+    if (issues.length === 0) {
+      console.log('go');
     } else {
-      console.log('No go');
+      console.log('nogo');
     }
   } catch (error) {
     console.error(error);
@@ -30,3 +25,4 @@ const checkIssues = async () => {
 };
 
 checkIssues();
+
