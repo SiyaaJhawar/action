e4b2edb812e7fdbacd23eb8840aba700622556e3
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-18 20:21:03 +0530

    Update defectid.js

--- GithubCommitComment/defectid.js
+++ GithubCommitComment/defectid.js
@@ -1,9 +1,5 @@
 import fetch from 'node-fetch';
 
-
-
-
-
 const owner = "SiyaaJhawar";
 const repo = "action";
 
@@ -15,27 +11,19 @@ const password = process.env.GITHUB_API_TOKEN;
 const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
 
 fetch(url, {
-  headers: {
-    
- "Authorization": `Basic ${encodedCredentials}`,
+  headers: {    
+    "Authorization": `Basic ${encodedCredentials}`,
     "Accept": "application/vnd.github.v3+json"
   }
-
 })
-
- .then(response => response.json())
+  .then(response => response.json())
   .then(data => {
     console.log(data);
-  const commentTexts = data.map(comment => comment.body);
-  const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
-  const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
-  console.log(defectIds);
-  const commaSeparatedIds = defectIds.join(', ');
-
-console.log(commaSeparatedIds);
- 
-
-
-
+    const commentTexts = data.map(comment => comment.body);
+    const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
+    const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
+    console.log(defectIds);
+    const commaSeparatedIds = defectIds.join(', ');
+    console.log(commaSeparatedIds);
   })
   .catch(error => console.error(error));
