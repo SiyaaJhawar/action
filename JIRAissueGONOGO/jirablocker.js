9ebc297bf7d5e1bdb0e01de35e5b4327966a01d7
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-18 11:00:04 +0530

    Update jirablocker.js

--- JIRAissueGONOGO/jirablocker.js
+++ JIRAissueGONOGO/jirablocker.js
@@ -3,17 +3,14 @@ import fetch from 'node-fetch';
 
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraApiToken = process.env.JIRA_API_TOKEN;
-
-
 const jira_url = process.env.JIRA_URL;
 const auth = 'Basic ' + Buffer.from(`${jiraUsername}:${jiraApiToken}`).toString('base64');
 
-const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
-
 const checkIssues = async () => {
-   const jql = process.env.INPUT_JQL
+  const jql = process.env.INPUT_JQL ;
+  // use the jql input value instead of the hardcoded value
+  const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
   try {
-   
     const response = await fetch(`${jira_url}?jql=${jql}`, { headers });
     const json = await response.json();
 
@@ -30,4 +27,3 @@ const checkIssues = async () => {
 };
 
 checkIssues();
-
