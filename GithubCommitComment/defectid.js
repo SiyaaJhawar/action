2c21c7409fd8ca7e751731fe3611acbc2ec04c4d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-19 12:44:51 +0530

    Update defectid.js

--- GithubCommitComment/defectid.js
+++ GithubCommitComment/defectid.js
@@ -8,19 +8,17 @@ const url = process.env.INPUT_GITHUB_URL;
 const username = process.env.GITHUB_USERNAME;
                                             
 const password = process.env.GITHUB_API_TOKEN;
+
 const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
 
-export async function getDefectIds(url, username, password) {
-  
-
-  try {
-    const response = await fetch(url, {
-      headers: {
-        "Authorization": `Basic ${encodedCredentials}`,
-        "Accept": "application/vnd.github.v3+json"
-      }
-    });
-    const data = await response.json();
+fetch(url, {
+  headers: {    
+    "Authorization": `Basic ${encodedCredentials}`,
+    "Accept": "application/vnd.github.v3+json"
+  }
+})
+  .then(response => response.json())
+  .then(data => {
     console.log(data);
     const commentTexts = data.map(comment => comment.body);
     const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
@@ -28,11 +26,7 @@ export async function getDefectIds(url, username, password) {
     console.log(defectIds);
     const commaSeparatedIds = defectIds.join(', ');
     console.log(commaSeparatedIds);
-    return defectIds;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
+  })
+  .catch(error => console.error(error));
 
 
