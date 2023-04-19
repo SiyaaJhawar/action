f22bdd85ed09cdd29b65f795dc139e2fa2c968c0
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-19 16:13:02 +0530

    Update defectid.js

--- GithubCommitComment/defectid.js
+++ GithubCommitComment/defectid.js
@@ -1,33 +1,33 @@
 import fetch from 'node-fetch';
 
-const getDefectIds = async () => {
-  const owner = "SiyaaJhawar";
-  const repo = "action";
-  const url = process.env.INPUT_GITHUB_URL;
-  const username = process.env.GITHUB_USERNAME;
-  const password = process.env.GITHUB_API_TOKEN;
-  const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
+const owner = "SiyaaJhawar";
+const repo = "action";
 
-  try {
-    const response = await fetch(url, {
-      headers: {    
-        "Authorization": `Basic ${encodedCredentials}`,
-        "Accept": "application/vnd.github.v3+json"
-      }
-    });
+const url = process.env.INPUT_GITHUB_URL;
 
-    const data = await response.json();
+const username = process.env.GITHUB_USERNAME;
+                                            
+const password = process.env.GITHUB_API_TOKEN;
+const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
+
+fetch(url, {
+  headers: {    
+    "Authorization": `Basic ${encodedCredentials}`,
+    "Accept": "application/vnd.github.v3+json"
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
     const commentTexts = data.map(comment => comment.body);
     const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
     const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
+    console.log(defectIds);
     const commaSeparatedIds = defectIds.join(', ');
-    return commaSeparatedIds;
-  } catch (error) {
-    console.error(error);
-  }
-};
+    console.log(commaSeparatedIds);
+  })
+  .catch(error => console.error(error));
 
-export { getDefectIds };
 
 
 
