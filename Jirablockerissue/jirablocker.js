28b6950d1d0aa8b7efbb2132e8c4f3881a04c6ee
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-20 18:02:28 +0530

    Update jirablocker.js

--- Jirablockerissue/jirablocker.js
+++ Jirablockerissue/jirablocker.js
@@ -2,29 +2,30 @@ import fetch from 'node-fetch';
 
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraApiToken = process.env.JIRA_API_TOKEN;
-const jiraUrl = process.env.INPUT_JIRA_URL;
-const jql = process.env.INPUT_JQL;
+const jira_url = process.env.INPUT_JIRA_URL;
 const auth = 'Basic ' + Buffer.from(`${jiraUsername}:${jiraApiToken}`).toString('base64');
 
 const checkIssues = async () => {
+  const jql = process.env.INPUT_JQL ;
+  // use the jql input value instead of the hardcoded value
   const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
   try {
-    const response = await fetch(`${jiraUrl}?jql=${jql}`, { headers });
+    const response = await fetch(`${jira_url}?jql=${jql}`, { headers });
     const json = await response.json();
 
     let result;
-    let issuesLength = 0;
-    let jsonKeys = [];
-
-    if (json.issues === undefined || json.issues.length === 0 || Object.keys(json).length === 0) {
+    if (json.issues === undefined || json.issues.length === 0) {
+      result = 'GO';
+    } else if (Object.keys(json).length === 0) {
       result = 'GO';
     } else {
       result = 'NOGO';
-      issuesLength = json.issues.length;
-      jsonKeys = Object.keys(json);
     }
-
-    return { result, issuesLength, jsonKeys };
+    return {
+      result: result,
+      issuesLength: json.issues ? json.issues.length : 0,
+      jsonKeys: Object.keys(json),
+    };
   } catch (error) {
     console.error(error);
     return { error: error.message };
@@ -32,14 +33,11 @@ const checkIssues = async () => {
 };
 
 const main = async () => {
-  const { result, issuesLength, jsonKeys } = await checkIssues();
-  console.log('Result:', result);
-  console.log('Number of issues:', issuesLength);
-  console.log('JSON keys:', jsonKeys);
+  const output = await checkIssues();
+  const outputJson = JSON.stringify(output);
+  console.log(outputJson);
+  // You can now use the outputJson variable to pass the JSON-formatted output to another function or module
 };
 
 main();
-
-
-
 
