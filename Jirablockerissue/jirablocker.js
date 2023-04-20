e89449edbc9d58c658e85c3a71d31cee3fd08934
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-20 18:49:52 +0530

    Update jirablocker.js

--- Jirablockerissue/jirablocker.js
+++ Jirablockerissue/jirablocker.js
@@ -2,24 +2,23 @@ import fetch from 'node-fetch';
 
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraApiToken = process.env.JIRA_API_TOKEN;
-const jira_url = process.env.INPUT_JIRA_URL;
+const jiraUrl = process.env.INPUT_JIRA_URL;
 const auth = 'Basic ' + Buffer.from(`${jiraUsername}:${jiraApiToken}`).toString('base64');
 
 const checkIssues = async () => {
-
-  // use the jql input value instead of the hardcoded value
+  const jql = process.env.INPUT_JQL;
   const headers = { 'Authorization': auth, 'Content-Type': 'application/json' };
+  
   try {
-    const response = await fetch(jira_url, {
-  method: 'POST',
-  headers,
-  body: JSON.stringify({ jql })
-});
+    const response = await fetch(jiraUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ jql })
+    });
+    
     const json = await response.json();
 
-   if (json.issues === undefined || json.issues.length === 0) {
-      console.log('GO');
-    } else if (Object.keys(json).length === 0) {
+    if (json.issues === undefined || json.issues.length === 0) {
       console.log('GO');
     } else {
       console.log('NOGO');
